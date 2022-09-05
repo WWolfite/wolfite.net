7f197d9db2fc23fe61f18304efa83698348bc037
WWolfite <100052562+WWolfite@users.noreply.github.com>
2022-09-05 18:25:01 +1000

    Create JoinBadge.lua

---
+++ rss/scripts/JoinBadge.lua
@@ -0,0 +1,9 @@
+local function awardBadge(plr, id)
+	if not game:GetService("BadgeService"):UserHasBadgeAsync(plr.UserId, 2128402179) then
+		game:GetService("BadgeService"):AwardBadge(plr.UserId, 2128402179)
+	end
+end
+
+game.Players.PlayerAdded:Connect(function(plr)
+	awardBadge(plr, 2128402179)
+end)
