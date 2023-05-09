8d2b8e70264c92f266c273962aafbc22ae6ff1a1
Wolfite <100052562+WWolfite@users.noreply.github.com>
2023-05-09 20:26:18 +1000

    Create wos.js

---
+++ wos/c/wos.js
@@ -0,0 +1,7 @@
+function to_basic() {
+    window.location.href = "https://wolfite.net/wos/t/basic";
+}
+
+function to_premium() {
+    window.location.href = "https://wolfite.net/wos/t/premium";
+}
